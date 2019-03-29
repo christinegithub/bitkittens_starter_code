128713a570af22fc93fdfefe096ed50f38d5996c
christinegithub <christine13@gmail.com>
2019-03-29 15:04:04 -0400

    exercise complete

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -1,2 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function() {
+
+  const button = document.querySelector('.summon-cats');
+  const catBoxes = document.querySelectorAll('.cat-box');
+
+  button.addEventListener('click', () => {
+    console.log('Button was clicked!');
+    axios.get('http://bitkittens.herokuapp.com/cats.json')
+    .then((response) => {
+      // console.log(response.data)
+
+    catBoxes.forEach((box, i) => {
+      box.innerHTML = '';
+      const image = document.createElement('img');
+      image.src = response.data.cats[i].photo;
+      image.alt = `Photo of ${response.data.cats[i].name}`;
+      box.appendChild(image);
+    });
+  });
+  });
+
 });
